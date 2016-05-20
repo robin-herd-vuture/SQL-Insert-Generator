48a9e8182530bf5de9d8580017d1f28690ce0d20
Robin Herd <robin.herd@vutu.re>
2016-05-20 15:57:28 +0100

    build up full insert statements

--- SqlInsertGenerator/Program.cs
+++ SqlInsertGenerator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using SqlInsertGenerator.Configuration;
 
@@ -18,19 +19,29 @@ namespace SqlInsertGenerator
             var first = lines.FirstOrDefault();
             var config = new ConfigBuilder().Build(first, table, db);
 
-            var headingsList = GetHeadingsList(config);
+            var statements = GetAllStatements(config, lines);
+        }
 
+        static IEnumerable<string> GetAllStatements(Config config, IEnumerable<string> lines)
+        {
+            var insertStatement = GetInsertStatement(config);
             foreach (var line in lines)
             {
                 var values = line.Split(',');
+                var formattedValues = new List<string>();
                 for (var i = 0; i < values.Length; i++)
                 {
                     var heading = config.ColumnHeadings.ElementAt(i);
                     if (!heading.Ignore)
                     {
                         var value = values[i];
+                        formattedValues.Add(FormatValue(value));
                     }
                 }
+
+                var valueStatement = string.Format("({0})", string.Join(", ", formattedValues));
+                var fullStatement = insertStatement + valueStatement;
+                yield return fullStatement;
             }
         }
 
@@ -40,11 +51,12 @@ namespace SqlInsertGenerator
             return string.Format("'{0}'", replaced);
         }
 
-        // e.g. "([EmailId], [FieldName], [FieldValue])"
-        static string GetHeadingsList(Config config)
+        // e.g. "INSERT INTO [Robin].[dbo].[Names] ([EmailId], [FieldName], [FieldValue]) VALUES "
+        static string GetInsertStatement(Config config)
         {
             var headings = config.ColumnHeadings.Where(x => !x.Ignore).Select(x => string.Format("[{0}]", x.Name));
-            return string.Format("({0})", string.Join(", ", headings));
+            var joinedHeadings = string.Join(", ", headings);
+            return string.Format("INSERT INTO [{0}].[dbo].[{1}] ({2}) VALUES ", config.DatabaseName, config.TableName, joinedHeadings);
         }
     }
 }
